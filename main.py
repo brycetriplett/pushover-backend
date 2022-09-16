0db201f68fe2bcdad5e3774bb1a6ecd61df6af88
Bryce Triplett <brycetriplett@gmail.com>
2022-09-16 14:02:44 -0700

    .

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ app = Flask(__name__)
 
 @app.route('/temperature-alert', methods=["POST"])
 def hello_world():
-    for x in request:
-        print(x)
-    # print(request.headers)
-    # return ''
+    # for x in request:
+    #     print(x)
+    print(request.data)
+    return ''
 
 if __name__ == '__main__':
    app.run(debug=True, host='0.0.0.0')
