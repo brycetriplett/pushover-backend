8bf5f73e7f50d7babed3ae4bc502cdb76a5bb64c
Bryce Triplett <brycetriplett@gmail.com>
2022-09-16 13:10:45 -0700

    added flask to main

---
+++ main.py
@@ -0,0 +1,10 @@
+from flask import Flask, request
+app = Flask(__name__)
+
+@app.route('/temperature-alert')
+def hello_world():
+    print(request)
+    return ''
+
+if __name__ == '__main__':
+   app.run(debug=True, host='0.0.0.0')
